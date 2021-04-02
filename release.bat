d0d88e00d0a26897ae40c49aef87fb7cee0cb44f
abc0922001 <abc0922001@hotmail.com>
2021-04-02 23:00:02 +0800

    release.bat support TongWenTang Extension Version 2.1.0

--- release.bat
+++ release.bat
@@ -17,11 +17,11 @@ rem ==================
 setlocal enabledelayedexpansion
 cd /d %~dp0
 if exist %ReleaseFile% del /q %ReleaseFile%
->>%ReleaseFile% set /p="{"version":"","autoConvert":"none","iconAction":"auto","symConvert":true,"inputConvert":"none","fontCustom":{"enable":false,"trad":"PMingLiU,MingLiU,新細明體,細明體","simp":"MS Song,宋体,SimSun"},"urlFilter":{"enable":false,"list":[]},"userPhrase":{"enable":true,"trad":{"  <nul 
 for /f "tokens=1*" %%i in (%IniFile%) do (
 rem ===複製資料夾===
 
->>%ReleaseFile% set /p=""%%i":"%%j","  <nul 
+>>%ReleaseFile% set /p=""%%i": "%%j","<nul
+>>%ReleaseFile% echo.<nul
 )
->>%ReleaseFile% set /p=""分屏":"分割螢幕"},"simp":{}},"contextMenu":{"enable":true}}"  <nul 
+>>%ReleaseFile% set /p=""分屏":"分割螢幕"  <nul 
 rem ==================
