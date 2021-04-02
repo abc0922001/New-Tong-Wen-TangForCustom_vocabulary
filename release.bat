3d0294448afd23071aa0e79f7652708a45a6bcda
abc0922001 <abc0922001@hotmail.com>
2021-04-02 23:26:39 +0800

    release.bat support TongWenTang Extension Version 2.1.0

--- release.bat
+++ release.bat
@@ -1,7 +1,7 @@
 echo off
 rem "Member Fields"
 set IniFile=0IniFile.ini 
-set ReleaseFile=README.md
+set ReleaseFile=tongwentang-pref.json
 chcp 65001
 rem ==================
 
@@ -17,11 +17,111 @@ rem ==================
 setlocal enabledelayedexpansion
 cd /d %~dp0
 if exist %ReleaseFile% del /q %ReleaseFile%
+
+rem header
+>>%ReleaseFile% set /p="{"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  "filter": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "enabled": true,"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "rules": []"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  },"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  "general": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "autoConvert": "disabled","<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "browserAction": "auto","<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "debugMode": false,"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "defaultTarget": "s2t","<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "spaMode": true"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  },"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  "menu": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "enabled": true,"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "group": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      "textarea": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="        "s2t": true,"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="        "t2s": true"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      },"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      "webpage": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="        "s2t": true,"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="        "t2s": true"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      }"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    }"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  },"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  "meta": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "update": 1617374678851"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  },"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  "version": 2,"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  "word": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "custom": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      "s2t": {"<nul
+>>%ReleaseFile% echo.<nul
+
 for /f "tokens=1*" %%i in (%IniFile%) do (
-rem ===複製資料夾===
+rem ===release===
 
 >>%ReleaseFile% set /p=""%%i": "%%j","<nul
 >>%ReleaseFile% echo.<nul
 )
->>%ReleaseFile% set /p=""分屏":"分割螢幕"  <nul 
+
+rem boom
+>>%ReleaseFile% set /p=""分屏": "分割螢幕""<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      },"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      "t2s": {}"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    },"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    "default": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      "s2t": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="        "char": true,"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="        "phrase": true"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      },"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      "t2s": {"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="        "char": true,"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="        "phrase": true"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="      }"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="    }"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="  }"<nul
+>>%ReleaseFile% echo.<nul
+>>%ReleaseFile% set /p="}"<nul
+>>%ReleaseFile% echo.<nul
 rem ==================
